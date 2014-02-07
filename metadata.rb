fdbb34f60b3ab289b3cf974a96d64a26c7014db6
Edward Paget <ed.paget@gmail.com>
2014-02-07 13:48:29 -0600

    Fix typo in storm.yaml

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ed@zooniverse.org'
 license          'Apache Public License see COPYING'
 description      'Installs/Configures storm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'zeromq'
 depends 'java'
